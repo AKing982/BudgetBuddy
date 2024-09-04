e4a475e81c2be86f924ed6eae73e77691061660a
AKing982 <59631931+AKing982@users.noreply.github.com>
2024-09-04 05:54:39 -0600

    commented out jacksonconfig

--- src/main/java/com/app/budgetbuddy/config/JacksonConfig.java
+++ src/main/java/com/app/budgetbuddy/config/JacksonConfig.java
@@ -22,31 +22,31 @@ public class JacksonConfig
     private static final String DATE_FORMAT = "yyyy-MM-dd";
     private static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
-    @Bean
-    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer(){
-        return builder -> {
-            builder.simpleDateFormat(DATE_TIME_FORMAT);
-            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(DATE_FORMAT)));
-            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)));
-            builder.deserializers(new LocalDateDeserializer(DateTimeFormatter.ofPattern(DATE_FORMAT)));
-            builder.deserializers(new CustomLocalDateDeserializer());
-        };
-    }
-
-    public static class CustomLocalDateDeserializer extends JsonDeserializer<LocalDate> {
-        @Override
-        public LocalDate deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
-            JsonNode node = jp.getCodec().readTree(jp);
-            if (node.isObject()) {
-                int year = node.get("year").asInt();
-                int month = node.get("month").asInt();
-                int day = node.get("day").asInt();
-                return LocalDate.of(year, month, day);
-            } else if (node.isTextual()) {
-                String dateString = node.asText();
-                return LocalDate.parse(dateString, DateTimeFormatter.ofPattern(DATE_FORMAT));
-            }
-            throw new IllegalArgumentException("Unable to deserialize LocalDate");
-        }
-    }
+//    @Bean
+//    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer(){
+//        return builder -> {
+//            builder.simpleDateFormat(DATE_TIME_FORMAT);
+//            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(DATE_FORMAT)));
+//            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)));
+//            builder.deserializers(new LocalDateDeserializer(DateTimeFormatter.ofPattern(DATE_FORMAT)));
+//            builder.deserializers(new CustomLocalDateDeserializer());
+//        };
+//    }
+//
+//    public static class CustomLocalDateDeserializer extends JsonDeserializer<LocalDate> {
+//        @Override
+//        public LocalDate deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+//            JsonNode node = jp.getCodec().readTree(jp);
+//            if (node.isObject()) {
+//                int year = node.get("year").asInt();
+//                int month = node.get("month").asInt();
+//                int day = node.get("day").asInt();
+//                return LocalDate.of(year, month, day);
+//            } else if (node.isTextual()) {
+//                String dateString = node.asText();
+//                return LocalDate.parse(dateString, DateTimeFormatter.ofPattern(DATE_FORMAT));
+//            }
+//            throw new IllegalArgumentException("Unable to deserialize LocalDate");
+//        }
+//    }
 }
