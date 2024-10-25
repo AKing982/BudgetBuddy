82046af4baf1bae6d124aa7b2a74659b8c15dcc8
AKing982 <59631931+AKing982@users.noreply.github.com>
2024-10-25 07:47:34 -0600

    commented out user category rule entity and added methods to budget calculator

--- src/main/java/com/app/budgetbuddy/entities/UserCategoryRuleEntity.java
+++ src/main/java/com/app/budgetbuddy/entities/UserCategoryRuleEntity.java
@@ -2,29 +2,29 @@ package com.app.budgetbuddy.entities;
 
 import jakarta.persistence.*;
 
-@Entity
-@Table(name="user_category_rules")
-public class UserCategoryRuleEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="id")
-    private UserEntity user;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="categoryid")
-    private CategoryEntity category;
-
-    @Column(name="merchantPattern")
-    private String merchantPattern;
-
-    @Column(name="descriptionPattern")
-    private String descriptionPattern;
-
-
-
-
-}
+//@Entity
+//@Table(name="user_category_rules")
+//public class UserCategoryRuleEntity {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name="id")
+//    private UserEntity user;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name="categoryid")
+//    private CategoryEntity category;
+//
+//    @Column(name="merchantPattern")
+//    private String merchantPattern;
+//
+//    @Column(name="descriptionPattern")
+//    private String descriptionPattern;
+//
+//
+//
+//
+//}
--- src/main/java/com/app/budgetbuddy/workbench/budget/BudgetCalculator.java
+++ src/main/java/com/app/budgetbuddy/workbench/budget/BudgetCalculator.java
@@ -1,9 +1,6 @@
 package com.app.budgetbuddy.workbench.budget;
 
-import com.app.budgetbuddy.domain.Budget;
-import com.app.budgetbuddy.domain.BudgetPeriod;
-import com.app.budgetbuddy.domain.BudgetStats;
-import com.app.budgetbuddy.domain.Category;
+import com.app.budgetbuddy.domain.*;
 import com.app.budgetbuddy.entities.BudgetCategoriesEntity;
 import com.app.budgetbuddy.entities.BudgetEntity;
 import com.app.budgetbuddy.services.BudgetCategoriesService;
@@ -87,12 +84,84 @@ public class BudgetCalculator
         return null;
     }
 
+    private void validateTotalCategoryPercentages(Category category, BigDecimal categoryPercentage, BigDecimal savingsPercentage, List<Category> allCategories)
+    {
+
+    }
+
+    private BigDecimal getDefaultPercentageForCategory(Category category, Budget budget, BigDecimal savingsTargetAmount){
+        switch(category.getCategoryType())
+        {
+            case AUTO -> {
+                return new BigDecimal("0.10");
+            }
+            case MEDICAL -> {
+                return new BigDecimal("0.05");
+            }
+            case RENT -> {
+
+            }
+        }
+        return new BigDecimal("0.01");
+    }
+
+    private BigDecimal getBudgetControlAmount(final Category category, final List<BudgetCategoriesEntity> budgetCategories)
+    {
+        for(BudgetCategoriesEntity budgetCategoriesEntity : budgetCategories)
+        {
+            if(budgetCategoriesEntity.getCategoryName().equalsIgnoreCase(category.getCategoryName()))
+            {
+                return BigDecimal.valueOf(budgetCategoriesEntity.getAllocatedAmount());
+            }
+        }
+        return category.getSpentOnCategory();
+    }
+
+    public BigDecimal getTotalSavedInCategories(List<Category> categories, BudgetPeriod budgetPeriod){
+        return null;
+    }
+
     public BigDecimal generateCategoryBudgetAmount(final Category category, final Budget budget, final BudgetPeriod budgetPeriod){
         if(category == null || budgetPeriod == null){
             throw new IllegalArgumentException("Invalid category or budget period");
         }
-        if(budget == null){
+        if(budget == null)
+        {
             throw new IllegalArgumentException("Invalid budget");
+        }
+        BudgetEntity budgetEntity = getBudgetEntityById(budget.getId());
+        if(budgetEntity == null)
+        {
+            throw new IllegalArgumentException("Budget not found.");
+        }
+        String budgetDescription = budget.getBudgetDescription().trim();
+        if(budgetDescription.equalsIgnoreCase("Budget Control Spending"))
+        {
+            List<BudgetCategoriesEntity> budgetCategoriesEntity = getBudgetCategoriesByBudgetId(budget.getId());
+            if(budgetCategoriesEntity.isEmpty())
+            {
+                return BigDecimal.ZERO;
+            }
+            else
+            {
+                for(BudgetCategoriesEntity budgetCategory : budgetCategoriesEntity)
+                {
+                    if(budgetCategory.getCategoryName().equalsIgnoreCase(category.getCategoryName()))
+                    {
+                        Double allocatedAmount = budgetCategory.getAllocatedAmount();
+                        return BigDecimal.valueOf(allocatedAmount);
+                    }
+                    else
+                    {
+                        // Get the total spend of the category
+                        return category.getSpentOnCategory();
+                    }
+                }
+            }
+        }
+        else if(budgetDescription.equalsIgnoreCase("Savings Budget"))
+        {
+
         }
 
         // Does the user already have a budget?
@@ -100,11 +169,18 @@ public class BudgetCalculator
            // if the budget is a spending budget
            // Does the user have budget category that matches the category?
              // If yes, then use the allocated amount as the budget amount
-            // If no, then use a percentage of the budget amount as the allocated amount
+            // If no, then use a percentage of the budget as the allocated amount
+            // What's the total spend on the category
+            // Get the average of the total spend on the category
+            // Return the average spend as the allocated amount
+        // Is the budget a Savings Budget?
+            // Get the total budget amount
+            //
+
         // Is the budget a spending control budget?
         // Is the budget a paying off debt budget?
 
-        return null;
+        return BigDecimal.ZERO;
     }
 
     @Deprecated
