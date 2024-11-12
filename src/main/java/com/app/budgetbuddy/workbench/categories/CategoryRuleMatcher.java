a33b7f034578d88732c2a887de1bdb13cddc1491
AKing982 <59631931+AKing982@users.noreply.github.com>
2024-11-12 07:45:18 -0700

    added code to category rule matcher and rule service

--- src/main/java/com/app/budgetbuddy/workbench/categories/CategoryRuleMatcher.java
+++ src/main/java/com/app/budgetbuddy/workbench/categories/CategoryRuleMatcher.java
@@ -2,15 +2,61 @@ package com.app.budgetbuddy.workbench.categories;
 
 import com.app.budgetbuddy.domain.CategoryRule;
 import com.app.budgetbuddy.domain.Transaction;
+import com.app.budgetbuddy.domain.UserCategoryRule;
+import com.app.budgetbuddy.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CategoryRuleMatcher
 {
-    public Boolean ruleMatchesTransaction(CategoryRule categoryRule, Transaction transaction){
+    private CategoryRuleService categoryRuleService;
+    private CategoryService categoryService;
+    private List<UserCategoryRule> userCategoryRules;
+    private List<CategoryRule> systemCategoryRules;
+
+    @Autowired
+    public CategoryRuleMatcher(CategoryRuleService categoryRuleService, CategoryService categoryService)
+    {
+        this.categoryRuleService = categoryRuleService;
+        this.categoryService = categoryService;
+        this.userCategoryRules = new ArrayList<UserCategoryRule>();
+        this.systemCategoryRules = new ArrayList<>();
+    }
+
+    /**
+     * Loads all the non user defined category rules from the database
+     */
+    public void loadCategoryRules()
+    {
+
+    }
+
+    /**
+     * Loads the user category rules by a particular userId
+     * @param userId
+     */
+    public void loadUserCategoryRules(Long userId)
+    {
+
+    }
+
+    public String categorizeTransactionByCustomRule(Transaction transaction, UserCategoryRule userCategoryRule)
+    {
         return null;
+    }
+
+    public String categorizeTransaction(Transaction transaction)
+    {
+        return "";
+    }
+
+    private boolean matchesRule(Transaction transaction, CategoryRule categoryRule)
+    {
+        return false;
     }
 
     public List<CategoryRule> getMatchingCategoryRulesForTransaction(Transaction transaction)
--- src/main/java/com/app/budgetbuddy/workbench/categories/CategoryRuleServiceImpl.java
+++ src/main/java/com/app/budgetbuddy/workbench/categories/CategoryRuleServiceImpl.java
@@ -1,17 +1,13 @@
 package com.app.budgetbuddy.workbench.categories;
 
-import com.app.budgetbuddy.domain.Category;
 import com.app.budgetbuddy.domain.CategoryRule;
-import com.app.budgetbuddy.domain.UserCategoryRule;
 import com.app.budgetbuddy.entities.CategoryRuleEntity;
 import com.app.budgetbuddy.repositories.CategoryRuleRepository;
-import com.plaid.client.model.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Service
