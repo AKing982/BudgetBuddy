08cfb003b72daf31bb355fe343c4d1a015b04ecb
AKing982 <59631931+AKing982@users.noreply.github.com>
2025-01-15 07:43:23 -0700

    added comments

--- src/main/java/com/app/budgetbuddy/workbench/budget/BudgetScheduleEngine.java
+++ src/main/java/com/app/budgetbuddy/workbench/budget/BudgetScheduleEngine.java
@@ -4,8 +4,13 @@ import com.app.budgetbuddy.domain.Budget;
 import com.app.budgetbuddy.domain.BudgetSchedule;
 import com.app.budgetbuddy.domain.DateRange;
 
+import java.util.List;
+
 /**
- * This class will implement logic for building both past and future Budget Schedules
+ * This class will implement logic for building both past and future Budget Schedules\
+ * This class will handle building Budget Schedules that can span over normal month ranges,
+ * or custom ranges depending on the particular budget plan
+ * This class will also need to build BudgetSchedules for up to 1-2 months in the future and also build budget schedules for past or previous periods
  */
 public class BudgetScheduleEngine
 {
@@ -13,7 +18,7 @@ public class BudgetScheduleEngine
      * This method will build monthly budget schedules
      *
      */
-    public BudgetSchedule createMonthlyBudgetSchedule(Budget budget, DateRange dateRange)
+    public BudgetSchedule createMonthlyBudgetSchedule(final Budget budget, final DateRange dateRange)
     {
         return null;
     }
@@ -21,4 +26,6 @@ public class BudgetScheduleEngine
     /**
      *
      */
+
+    public List<BudgetSchedule> createCustomBudgetSchedule(Budget budget, )
 }
